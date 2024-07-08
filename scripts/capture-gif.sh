7315930b0118478ef6ade3da80d074a1b4a59f0e
Wolf Mermelstein <wolfmermelstein@gmail.com>
2024-07-08 15:21:29 -0400

    Update gif to toggle

--- scripts/capture-gif.sh
+++ scripts/capture-gif.sh
@@ -1,7 +1,7 @@
 # If already running, kill wf_recorder and exit
 if pgrep -x "wf_recorder" > /dev/null
 then
-    pkill -x wf_recorder
+    pkill -x wf-recorder
     exit
 fi
 
@@ -12,4 +12,4 @@ $wf_recorder -g "$REGION" -f "$FILEPATH/recording.mkv"
 $ffmpeg -i "$FILEPATH/recording.mkv" -vf "fps=$FPS,scale=640:-1:flags=lanczos" "$FILEPATH/frames/frame%04d.png"
 $gifski -o "$FILEPATH/output.gif" "$FILEPATH"/frames/frame*.png --fps "$FPS" --quality "$QUALITY"
 $wl_copy --type "text/uri-list" "file://$FILEPATH/output.gif"
-$notify "Successfully saved gif!" "The gif has been saved to ${OUTPATH:FILEPATH}"
+$notify "Successfully saved gif!" "The gif has been saved to ${FILEPATH/output.gif}"
