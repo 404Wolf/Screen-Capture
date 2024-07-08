df97145e7f13d575a0b77da8f975970ed1d84035
Wolf Mermelstein <wolfmermelstein@gmail.com>
2024-07-08 15:09:30 -0400

    Add grim

--- scripts/partial-screenshot.sh
+++ scripts/partial-screenshot.sh
@@ -1,4 +1,4 @@
 FILEPATH=/tmp/$(uuidgen)
-grim -g "$(slurp)" "$FILEPATH.png"
+$grim -g "$(slurp)" "$FILEPATH.png"
 $wl_copy --type "text/uri-list" "file://$FILEPATH.png"
 $notify "Successfully saved screen capture!" "The png has been saved to $FILEPATH"
